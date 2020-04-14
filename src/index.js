606b3b5d4b2648cda7804a479599cdba6d2d64eb
Rakeshgsekhar <rakeshgsekhar.fortune@gmail.com>
2020-04-14 20:51:29 +0530

    eslint config and index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
-function sayHi(){
-	return 'Hi';
-}
+export const formateDate = (date, mode) => {};
 
-const message = sayHi();
+export const currentDateFormater = (mode) => {
+  let currnetDate = new Date();
+  let month = ("0" + (currnetDate.getMonth() + 1)).slice(-2);
+  let day = ("0" + currnetDate.getDate()).slice(-2);
+  let year = "" + currnetDate.getFullYear();
 
-console.log(message);
+  const months = [
+    "JANUARY",
+    "FEBRUARY",
+    "MARCH",
+    "APRIL",
+    "JUNE",
+    "JULY",
+    "AUGUST",
+    "SEPTEMBER",
+    "OCTOBER",
+    "NOVEMBER",
+    "DECEMBER",
+  ];
+  let formateddate = "";
+  switch (mode) {
+    case "DDMMMYYYY": {
+      formateddate = day + " " + months[month].slice(3) + " " + year;
+      return formateddate;
+	}
+	default:{
+		return "invalid mode";
+	}
+  }
+};
